86b512647443ac85a794c18eeff1e15b0ed2e95c
Louis Mille <mille.louis1@gmail.com>
2023-07-11 16:38:49 +0200

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Louis Mille on 11/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

937ef6ce0f711c3956cde97052a4ee7227fd655f
Louis Mille <mille.louis1@gmail.com>
2023-07-14 00:23:23 +0200

    made some animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -7,22 +7,128 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
     
     @State private var animationCount = 0.0
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    let letters = Array("Hello, SwiftUI")
+    @State private var isEnabled = false
+    
+    @State private var isShowingRed = false
+
+    
     
     var body: some View {
-        
-        Button("Tap me") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                animationCount += 360
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
             }
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationCount), axis: (x: 0, y: 1, z: 0))
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
+        
+//        VStack {
+//            Button("Tap me") {
+//                withAnimation {
+//                    isShowingRed.toggle()
+//                }
+//            }
+//
+//            if isShowingRed {
+//                Rectangle()
+//                    .fill(.red)
+//                    .frame(width: 200, height: 200)
+//                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+//            }
+//        }
+        
+// Animate a string like a snake
+//        HStack(spacing: 0) {
+//            ForEach(0..<letters.count) { num in
+//                Text(String(letters[num]))
+//                    .padding(5)
+//                    .font(.title)
+//                    .background(enabled ? .blue : .red)
+//                    .offset(dragAmount)
+//                    .animation(.default.delay(Double(num) / 20), value: dragAmount)
+//
+//            }
+//        }
+//        .gesture(
+//            DragGesture()
+//                .onChanged { dragAmount = $0.translation }
+//                .onEnded { _ in
+//                    dragAmount = .zero
+//                    enabled.toggle()
+//
+//                }
+//        )
+        
+//        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+//            .frame(width: 300, height: 200)
+//            .clipShape(RoundedRectangle(cornerRadius: 10))
+//            .offset(dragAmount)
+//            .gesture(
+//                DragGesture()
+//                    .onChanged { dragAmount = $0.translation }
+//                    .onEnded { _ in dragAmount = .zero }
+//            )
+//        //add a delay to the animation
+//            .animation(.spring(), value: dragAmount)
+        
+        
+//        Button("Tap me") {
+//            enabled.toggle()
+//        }
+//        .frame(width: 200, height: 200)
+//        .background(enabled ? .blue : .red)
+//        .animation(.default, value: enabled)
+//        .foregroundColor(.white)
+//        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+//        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
+//
+        
+//        Button("Tap me") {
+//            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+//                animationCount += 360
+//            }
+//        }
+//        .padding(50)
+//        .background(.red)
+//        .foregroundColor(.white)
+//        .clipShape(Circle())
+//        .rotation3DEffect(.degrees(animationCount), axis: (x: 0, y: 1, z: 0))
         
         
         
